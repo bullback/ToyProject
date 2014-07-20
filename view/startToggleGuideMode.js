b3481ddc5d3fe33ec802cb18c1062ef37b2e6484
Wonkyung Lyu <me@lyuwonkyung.com>
2014-07-20 14:05:39 +0900

    Get rid of useless(commented) codes.

--- view/startToggleGuideMode.js
+++ view/startToggleGuideMode.js
@@ -5,26 +5,6 @@
 	$("html").ready(function(){
 
 
-		// chrome.tabs.getCurrent(function(tab) {
-
-
-		// 	console.log(tab);
-		// 	// if(tab.id != undefined){
-
-		// 	// 	chrome.tabs.executeScript(tab.id, {
-
-		// 	// 		code: "alert(dd);",
-		// 	// 		allFrames: true
-
-		// 	// 	});
-
-		// 	// }else{
-
-		// 	// 	alert('You cannot toggle to the Making Guide Mode on this Tab!');
-		// 	// }
-
-		// });
-
 		chrome.tabs.query({
 
 			active: true,
@@ -33,7 +13,6 @@
 
 			if(tab != undefined && tab.length > 0){
 
-				//console.log(tab[0].id);
 				chrome.tabs.executeScript(tab[0].id, {
 					code: "clickObject(); stopAtag(); drawShadowOnMouseOveredDOM();",
 					allFrames: true
